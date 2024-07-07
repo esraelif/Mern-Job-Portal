e9dde63373ed69c2a980b933a710968020ce392d
esraelif <elifesratunca@gmail.com>
2024-07-07 12:31:11 +0200

    first commit

---
+++ job-portal-client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+import './App.css'
+export default function App() {
+  return (
+    <>
+      <nav>Nav Bar</nav>
+      <Outlet />
+      <footer>Footer</footer>
+    </>
+
+  )
+}
