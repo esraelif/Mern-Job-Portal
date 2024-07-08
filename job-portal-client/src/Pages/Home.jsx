25910d87687d106dca2ece7701ac3f214a44aff5
esraelif <elifesratunca@gmail.com>
2024-07-08 10:56:10 +0200

    "Updated ESLint config to disable 'react/prop-types' rule and modified Home.jsx to import Banner component and add state for query input"

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+
+import Banner from '../components/Banner';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [query, setQuery] = useState("")
+    const handleInputChange = (event) => {
+        setQuery(event.target.value)
+    }
+    console.log(query)
     return (
-        <div className='text-blue'>
-            home
+        <div className=''>
+            <Banner query={query} handleInputChange={handleInputChange} />
         </div>
     );
 }
